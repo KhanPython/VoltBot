e5b0eb111623ed0c9cf69e9e9d6a5c54fa0d614d
KhanPython <81525782+KhanPython@users.noreply.github.com>
2022-09-08 04:31:48 +0500

    Update submitKeys.js

--- src/commands/submitKeys.js
+++ src/commands/submitKeys.js
@@ -59,7 +59,7 @@ module.exports = {
         .setTitle(`Server ${guild.id} secrets set`)
         .setColor("BLUE")
         .setDescription("✅ API keys successfully saved!")
-        .addField("❗REMINDER:" , "Never share your API keys with anyone! Doing so may completely break the experience.")
+        .addField("REMINDER:" , "Never share your API keys with anyone! Doing so may completely break your linked experience.")
         .setTimestamp()
 
         if (message) {
